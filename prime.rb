cdc85aa2752643aca43fb636a740e3d4edf51e32
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-11-13 20:12:12 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(integer)
+    if integer < 2
+      return false
+    else
+      testrange = 2..integer-1
+      testrange.each do |num|
+          if integer % num == 0
+              return false
+          end
+      end
+    return true
+    end
+end
+
+prime?(2)
